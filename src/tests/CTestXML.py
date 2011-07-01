3ce2974ff9ceae3d9b148f7cebd7b7c1d660416d
Hellmann <vladimir@fridu.net>
2011-07-01 11:52:48 +0200

    Gustav added XML structure check

--- src/tests/CTestXML.py
+++ src/tests/CTestXML.py
@@ -20,22 +20,35 @@ class CTestXML():
     def loadTrueFile(self):
         url="./XMLFileTest/input1.xml"
         aXML_Parse=XML_Parse.XML_Parse()
-        return aXML_Parse.ExistsXML(url)
+        return aXML_Parse.ExistsXMLVerif(url)
         
     def loadNoFile(self):
         url="./XMLFileTest/NONE.xml"
         aXML_Parse=XML_Parse.XML_Parse()
-        return aXML_Parse.ExistsXML(url)
+        return aXML_Parse.ExistsXMLVerif(url)
 
     def loadNonEmptyFile(self):
-        url="./XMLFileTest/input.xml"
+        url="./XMLFileTest/Empty.xml"
         aXML_Parse=XML_Parse.XML_Parse()
-        return aXML_Parse.EmptyXML(url)
+        return aXML_Parse.EmptyXMLVerif(url)
     
     def loadEmptyFile(self):
         url="./XMLFileTest/Empty.xml"
         aXML_Parse=XML_Parse.XML_Parse()
-        return aXML_Parse.EmptyXML(url)
+        return aXML_Parse.EmptyXMLVerif(url)
+    
+    def parseGoodFile(self):
+        url="./XMLFileTest/input.xml"
+        aXML_Parse=XML_Parse.XML_Parse()
+        return aXML_Parse.StructureXMLVerif(url)
+    
+    def parseBadFile(self):
+        url="./XMLFileTest/pourri.xml"
+        aXML_Parse=XML_Parse.XML_Parse()
+        return aXML_Parse.StructureXMLVerif(url)
+    
+    
+    
     
     
     
