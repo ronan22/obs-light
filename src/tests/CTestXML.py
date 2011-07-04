8a66abc683b17275748dcc017d2c07779d1f6c2e
Hellmann <vladimir@fridu.net>
2011-07-04 18:32:18 +0200

    modifications by Gustav

--- src/tests/CTestXML.py
+++ src/tests/CTestXML.py
@@ -73,10 +73,63 @@ class CTestXML():
     def convertBadDictToXML(self):
         # In BadDict when compared to GoodDict the "{" bracket before 'ARTIST' is missing
         
+        #alternatively: BadDict=5
         BadDict={}
         XMLOutFile="./XMLFileTest/OutpForBadDict.xml"
         aXML_Parse=XML_Parse.XML_Parse()
         return aXML_Parse.ConvertDictToXMLVerif(BadDict,XMLOutFile)
     
+    def modifyCorrectlyDict(self):
+        
+        MyDict = {'CATALOG': {'CD': [{'ARTIST': 'Bob Dylan',
+                                   'attr_My_attr1': 'My_attr1_test',
+                                   'Test_liste' :
+                                   [{'_text': 'My_text1_test'},
+                                   {'_text': 'My_text2_test'}],
+                                    'COMPANY': 'Columbia',
+                                    'COUNTRY': 'USA',
+                                    'PRICE': '10.90',
+                                    'TITLE': 'Empire Burlesque',
+                                    'YEAR': '1985'},
+                                   {'ARTIST': 'Bonnie Tyler',
+                                    'COMPANY': 'CBS Records',
+                                    'COUNTRY': 'UK',
+                                    'PRICE': '9.90',
+                                    'TITLE': 'Hide your heart',
+                                    'YEAR': '1988'}]}}       
+        
+        CorrectDictList = ['CATALOG', 'CD', 0, 'ARTIST']
+        
+        XMLOutDict = "./XMLFileTest/ModifyGoodDict"       
+        XMLOutFile = "./XMLFileTest/ModifyGoodDict.xml"
+        aXML_Parse=XML_Parse.XML_Parse()
+        return aXML_Parse.ModifDictVerif(MyDict,'ToryAmos',CorrectDictList)
+            
+    def modifyWronglyDict(self):
+        
+        MyDict = {'CATALOG': {'CD': [{'ARTIST': 'Bob Dylan',
+                                   'attr_My_attr1': 'My_attr1_test',
+                                   'Test_liste' :
+                                   [{'_text': 'My_text1_test'},
+                                   {'_text': 'My_text2_test'}],
+                                    'COMPANY': 'Columbia',
+                                    'COUNTRY': 'USA',
+                                    'PRICE': '10.90',
+                                    'TITLE': 'Empire Burlesque',
+                                    'YEAR': '1985'},
+                                   {'ARTIST': 'Bonnie Tyler',
+                                    'COMPANY': 'CBS Records',
+                                    'COUNTRY': 'UK',
+                                    'PRICE': '9.90',
+                                    'TITLE': 'Hide your heart',
+                                    'YEAR': '1988'}]}}       
+        
+        WrongDictList = ['CATALOG', 'CD', 'ARTIST']
+        XMLOutDict = "./XMLFileTest/ModifyGoodDict"       
+        XMLOutFile = "./XMLFileTest/ModifyGoodDict.xml"
+        aXML_Parse=XML_Parse.XML_Parse()
+        return aXML_Parse.ModifDictVerif(MyDict,'ToryAmos',WrongDictList)
+        
     
+       
     
