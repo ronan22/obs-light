43cb9c3b3b9c51013e532c7679fdcc7d8a15fcc7
Hellmann <vladimir@fridu.net>
2011-07-04 13:48:10 +0200

    modifications done by Gustav

--- src/OBSLight/XML_Filelists.py
+++ src/OBSLight/XML_Filelists.py
@@ -39,27 +39,18 @@ def main():
     DICT_FILE_NAME = '/home/hellmann/XML_files/output.dict' #The dictionary obtained from the XML file
     
     MyOutFName=OUT_XML_FILE_NAME
-    MyTempFName=MyOutFName.replace('.xml','.temp.xml') #The output XML file will be first written into a temp file
-      
     aFilelists_cli = XML_Filelists(IN_XML_FILE_NAME)  
     aFilelists_cli.parseXML(IN_XML_FILE_NAME)
-    
     #For eventual modifications of the dictionary
     #aFilelists_cli.modifyDict()
-    
     aFilelists_cli.setFilelistsNameSpaceDict(aFilelists_cli.getNameSpaceDict()) 
     aFilelists_cli.setFilelistsDicoStorage(aFilelists_cli.getDicoStorage())
     MyFilelistsDict = aFilelists_cli.getFilelistsDicoStorage()
     aFilelists_cli.printObj(DICT_FILE_NAME,MyFilelistsDict)
-    aFilelists_cli.dumpXML(MyTempFName)
+    aFilelists_cli.dumpXML(MyOutFName)
+    aFilelists_cli.lintXML(MyOutFName,MyOutFName)
        
-    #The xmllint command will format the output XML file, in particular newlines will be added if necessary    
-    MyCommand_1="xmllint --format '" + MyTempFName +  "' > '" + MyOutFName + "'"
-    MyCommand_2="rm -rf '" + MyTempFName + "'"
-    os.system(MyCommand_1)
-    os.system(MyCommand_2)
-   
-     
+    
 if __name__ == '__main__':
     main()
     
