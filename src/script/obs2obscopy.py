e198bea5cb06d726790d76e64ad8c5915100d3b1
(no author) <(no author)@ac3f0ef0-e81d-11e0-8956-a383e94ad1b7>
2011-10-03 09:47:45 +0000

    obs2obscopy.py: Added support for different source and destination OBS

--- src/script/obs2obscopy.py
+++ src/script/obs2obscopy.py
@@ -13,43 +13,58 @@ import subprocess
 
 def help(progName="obs2obscopy"):
     print """HELP
-Function : copy a project from an OBS to an OBS (remote obs are managed via a linked project)
+Function : copy a project from an OBS to an OBS.
            using as input a package list with the desired MD5 to represent the package revision
            if target pakage is already at the correct revision, its copy is ignored
            running the script several time is possible until zero errors is acheived
            A log file named $MD5_FILE.log is created for everyrun.
 
 Usage:     %s obs-alias source_prj target_prj md5_file
-           md5_file is create by the script obstag
+       or  %s obs-alias source_prj other-obs-alias target_prj md5_file
+           md5_file is created by the script obstag
 
 Example1: Local copy
-         %s http://myObs.mynetwork:81 MyMeeGo:1.2:oss myTest:MeeGo:1.2:oss my_revision_tag.md5
-Example2: Remote copy
-         %s http://myObs.mynetwork:81 meego.com:MeeGo:1.2:oss myobs:MeeGo:1.2:oss my_revision_tag.md5
+       %s http://myObs.mynetwork:81 MyMeeGo:1.2:oss myTest:MeeGo:1.2:oss my_revision_tag.md5
+Example2: Remote copy (by link)
+       %s http://myObs.mynetwork:81 meego.com:MeeGo:1.2:oss myobs:MeeGo:1.2:oss my_revision_tag.md5
            where meego.com is a local projet which is a link to a remote OBS public api
-Version 3.0  License GPLv2"""  % (progName, progName, progName)
+Example3: Remote copy
+       %s http://api.pub.meego.com Project:MINT:Testing http://myObs.mynetwork:81 myTest:MINT:Testing my_revision_tag.md5
 
-def main(apiUrl, srcProjectName, dstProjectName, tagFilePath):
+Return code:
+          0 success
+          1 wrong number of arguments
+          2 problem reading MD5 file
+          3 network error
+          4 interrupted by user
+
+Version 3.0  License GPLv2"""  % (progName, progName, progName, progName, progName)
+
+def main(apiUrl, srcProjectName, dstApiUrl, dstProjectName, tagFilePath):
     try:
         tagFile = open(tagFilePath, "ru")
     except IOError as e:
         print >> sys.stderr, "Cannot open", tagFilePath, ":", e.strerror
-        sys.exit(1)
+        sys.exit(2)
 
 
     print "info : Checking connectivity with remote source and local target ... "
-    # TODO: read stdout to check is osc is asking for credentials
-    osc = subprocess.Popen(["osc", "-A", apiUrl, "ls", dstProjectName], stdout=subprocess.PIPE)
+    # TODO: read stdout to check if osc is asking for credentials
+    osc = subprocess.Popen(["osc", "-A", dstApiUrl, "ls", dstProjectName],
+                           stdout=subprocess.PIPE)
     osc.communicate()
     if osc.returncode != 0:
         print >> sys.stderr, "Error: Local destination project 'osc -A %s ls %s' cannot be reached"\
             % (apiUrl, dstProjectName)
-    # TODO: read stdout to check is osc is asking for credentials
-    osc = subprocess.Popen(["osc", "-A", apiUrl, "ls", srcProjectName], stdout=subprocess.PIPE)
+        sys.exit(3)
+    # TODO: read stdout to check if osc is asking for credentials
+    osc = subprocess.Popen(["osc", "-A", apiUrl, "ls", srcProjectName],
+                           stdout=subprocess.PIPE)
     osc.communicate()
     if osc.returncode != 0:
         print >> sys.stderr, "Error: Remote project 'osc -A %s ls %s' cannot be reached"\
             % (apiUrl, srcProjectName)
+        sys.exit(3)
 
     for line in tagFile:
         parts = line.split("|", 4)
@@ -57,14 +72,16 @@ def main(apiUrl, srcProjectName, dstProjectName, tagFilePath):
         if parts[0].startswith("#"):
             continue
         pkgName = parts[3].strip()
-#        print "%40s : %s" % (pkgName, md5)
 
-        osc = subprocess.Popen(["osc", "-A", apiUrl, "ls", "--revision=%s" % md5, dstProjectName, pkgName],
+        osc = subprocess.Popen(["osc", "-A", dstApiUrl, "ls",
+                                "--revision=%s" % md5, dstProjectName, pkgName],
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         osc.communicate()
         if osc.returncode != 0:
             print "info : Copying %s from %s" % (pkgName, srcProjectName),
-            osc = subprocess.Popen(["osc", "-A", apiUrl, "copypac", "--revision=%s" % md5, srcProjectName, pkgName, dstProjectName],
+            osc = subprocess.Popen(["osc", "-A", apiUrl, "copypac",
+                                    "--revision=%s" % md5, srcProjectName,
+                                    pkgName, "-t", dstApiUrl, dstProjectName],
                                    stdout=subprocess.PIPE)
             osc.communicate()
             if osc.returncode == 0:
@@ -72,14 +89,22 @@ def main(apiUrl, srcProjectName, dstProjectName, tagFilePath):
             else:
                 print "FAILED"
         else:
-            print "info : %s is already present on %s" % (pkgName, dstProjectName)
+            print "info : %s is already present on %s" % (pkgName,
+                                                          dstProjectName)
     
-
     tagFile.close()
     
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        help(sys.argv[0])
-        sys.exit(1)
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    try:
+        if len(sys.argv) == 5:
+            # Use same OBS URL as source and destination
+            main(sys.argv[1], sys.argv[2], sys.argv[1], sys.argv[3], sys.argv[4])
+        elif len(sys.argv) == 6:
+            main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+        else:
+            help(sys.argv[0])
+            sys.exit(1)
+    except KeyboardInterrupt:
+        print >> sys.stderr, "Interrupted by user..."
+        sys.exit(4)
