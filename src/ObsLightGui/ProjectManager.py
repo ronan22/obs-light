a4455e9006728515adab8cff30b4515d54965e95
Florent Vennetier <florent@fridu.net>
2011-11-14 17:21:05 +0100

    - added import/export from GUI

--- src/ObsLightGui/ProjectManager.py
+++ src/ObsLightGui/ProjectManager.py
@@ -22,7 +22,7 @@ Created on 27 sept. 2011
  
 from PySide.QtCore import QObject, QRegExp, QThreadPool, Signal, Qt
 from PySide.QtGui import QTextEdit, QPushButton, QListWidget, QLineEdit, QLabel, QComboBox
-from PySide.QtGui import QRegExpValidator, QRadioButton, QProgressDialog
+from PySide.QtGui import QRegExpValidator, QRadioButton, QProgressDialog, QFileDialog
 
 from Utils import QRunnableImpl, ProgressRunnable, popupOnException
 from PackageManager import PackageManager
@@ -37,6 +37,8 @@ class ProjectManager(QObject):
     __newObsProjectButton = None
     __modifyObsProjectButton = None
     __deleteObsProjectButton = None
+    __importObsProjectButton = None
+    __exportObsProjectButton = None
     __createChrootButton = None
     __addRepoInChrootButton = None
     __importRpmButton = None
@@ -69,6 +71,12 @@ class ProjectManager(QObject):
         self.__deleteObsProjectButton = mainWindow.findChild(QPushButton,
                                                              "deleteObsProjectButton")
         self.__deleteObsProjectButton.clicked.connect(self.on_deleteObsProjectButton_clicked)
+        self.__importObsProjectButton = mainWindow.findChild(QPushButton,
+                                                             "importObsProjectButton")
+        self.__importObsProjectButton.clicked.connect(self.on_importObsProjectButton_clicked)
+        self.__exportObsProjectButton = mainWindow.findChild(QPushButton,
+                                                             "exportObsProjectButton")
+        self.__exportObsProjectButton.clicked.connect(self.on_exportObsProjectButton_clicked)
         self.__createChrootButton = mainWindow.findChild(QPushButton,
                                                          "createChrootButton")
         self.__createChrootButton.clicked.connect(self.on_createChrootButton_clicked)
@@ -128,6 +136,35 @@ class ProjectManager(QObject):
         self.__gui.getObsLightManager().removeProject(projectName)
         self.loadProjectList()
         self.on_projectSelected(None)
+
+    @popupOnException
+    def on_importObsProjectButton_clicked(self):
+        filePath, _filter = QFileDialog.getOpenFileName(self.__gui.getMainWindow(),
+                                                        "Select project to import")
+        obslightManager = self.__gui.getObsLightManager()
+        self.__progress.setLabelText("Importing project...")
+        self.__progress.show()
+        runnable = ProgressRunnable(obslightManager.importProject, filePath)
+        runnable.setProgressDialog(self.__progress)
+        runnable.finishedWithException.connect(self.__gui.obsLightErrorCallback2)
+        runnable.finished.connect(self.loadProjectList)
+        QThreadPool.globalInstance().start(runnable)
+
+    @popupOnException
+    def on_exportObsProjectButton_clicked(self):
+        project = self.getCurrentProjectName()
+        if project is None:
+            return
+        filePath, _filter = QFileDialog.getSaveFileName(self.__gui.getMainWindow(),
+                                                        "Select file to export")
+        self.__progress.setLabelText("Importing project...")
+        self.__progress.show()
+        obslightManager = self.__gui.getObsLightManager()
+        runnable = ProgressRunnable(obslightManager.exportProject, project, filePath)
+        runnable.setProgressDialog(self.__progress)
+        runnable.finishedWithException.connect(self.__gui.obsLightErrorCallback2)
+        runnable.finished.connect(self.loadProjectList)
+        QThreadPool.globalInstance().start(runnable)
 
     @popupOnException
     def on_projectConfigManager_finished(self, success):
@@ -209,6 +246,9 @@ class ProjectManager(QObject):
                                                                      projectObsName))
             self.__projectRepoLinkLabel.setText('<a href="%s">%s</a>' % (repoLink,
                                                                          target))
+            self.__exportObsProjectButton.setEnabled(True)
+        else:
+            self.__exportObsProjectButton.setEnabled(False)
 
     @popupOnException
     def updateChrootPathAndButtons(self):
