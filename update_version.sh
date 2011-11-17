d545cb89c05e1e0162197f4256e3288cbf7dfba5
Florent Vennetier <florent@fridu.net>
2011-11-17 12:16:30 +0100

    - updated update version script - updated version to 0.4.1 - fixed sudoers rule modification on package update

--- update_version.sh
+++ update_version.sh
@@ -8,27 +8,28 @@ fi
 OLDVERSION=$1
 NEWVERSION=$2
 
-grep --color "obslight ($OLDVERSION-1)" debian.changelog
-sed -i s/"obslight ($OLDVERSION-1)"/"obslight ($NEWVERSION-1)"/ debian.changelog
+grep --color "obslight ($OLDVERSION-1)" deb/debian.changelog
+sed -i s/"obslight ($OLDVERSION-1)"/"obslight ($NEWVERSION-1)"/ deb/debian.changelog
 echo "---"
-grep --color "obslight ($NEWVERSION-1)" debian.changelog
+grep --color "obslight ($NEWVERSION-1)" deb/debian.changelog
 echo "----------------"
-grep --color "Version: $OLDVERSION" obslight.dsc
-sed -i s/"Version: $OLDVERSION"/"Version: $NEWVERSION"/ obslight.dsc
+grep --color "Version: $OLDVERSION" deb/obslight.dsc
+sed -i s/"Version: $OLDVERSION"/"Version: $NEWVERSION"/ deb/obslight.dsc
 echo "---"
-grep --color "Version: $NEWVERSION" obslight.dsc
+grep --color "Version: $NEWVERSION" deb/obslight.dsc
 echo "----------------"
-grep --color "obslight_$OLDVERSION" obslight.dsc
-sed -i s/"obslight_$OLDVERSION"/"obslight_$NEWVERSION"/ obslight.dsc
+grep --color "obslight_$OLDVERSION" deb/obslight.dsc
+sed -i s/"obslight_$OLDVERSION"/"obslight_$NEWVERSION"/ deb/obslight.dsc
 echo "---"
-grep --color "obslight_$NEWVERSION" obslight.dsc
+grep --color "obslight_$NEWVERSION" deb/obslight.dsc
 echo "----------------"
-grep --color "version $OLDVERSION" obslight.spec
-sed -i s/"version $OLDVERSION"/"version $NEWVERSION"/ obslight.spec
+grep --color "version $OLDVERSION" rpm/obslight.spec
+sed -i s/"version $OLDVERSION"/"version $NEWVERSION"/ rpm/obslight.spec
 echo "---"
-grep --color "version $NEWVERSION" obslight.spec
+grep --color "version $NEWVERSION" rpm/obslight.spec
 echo "----------------"
 grep --color "version=\"$OLDVERSION\"" src/setup.py
 sed -i s/"version=\"$OLDVERSION\""/"version=\"$NEWVERSION\""/ src/setup.py
 echo "---"
 grep --color "version=\"$NEWVERSION\"" src/setup.py
+
