ea128e391fa9ad8ab0ddf7ef2a8db695be8a5568
Florent Vennetier <florent@fridu.net>
2011-12-13 16:17:29 +0100

    - added .gitignore at root - added getPackageFileInfo() in ObsLightManager - added sorting and column auto update in OscWorkingCopyModel

--- src/ObsLightGui/OscWorkingCopyModel.py
+++ src/ObsLightGui/OscWorkingCopyModel.py
@@ -30,7 +30,10 @@ class OscWorkingCopyModel(QAbstractTableModel):
     __project = None
     __package = None
     __columnList = None
+    __fileList = None
+    __fileInfos = None
 
+    sortKey = None
     sortOrder = Qt.SortOrder.AscendingOrder
 
     def __init__(self, obsLightManager, projectName, packageName):
@@ -40,6 +43,9 @@ class OscWorkingCopyModel(QAbstractTableModel):
         self.__package = packageName
         self.__columnList = list()
         self.clearColumns()
+        self.__fileList = list()
+        self.__fileInfos = dict()
+        self.refresh()
 
     def clearColumns(self):
         self.__columnList[:] = []
@@ -58,30 +64,45 @@ class OscWorkingCopyModel(QAbstractTableModel):
             else:
                 return self.__columnList[section]
 
-    def displayRoleData(self, index):
-        row = index.row()
-        column = index.column()
+    def displayRoleData(self, row, column):
+        fileName = self.fileList()[row]
         if column == 0:
-            return self.fileList()[row]
+            return fileName
+        elif column < self.columnCount():
+            return self.__fileInfos[fileName].get(self.__columnList[column])
         return None
 
     def data(self, index, role=Qt.DisplayRole):
         if not index.isValid() or index.row() >= self.rowCount():
             return None
         if role == Qt.DisplayRole:
-            return self.displayRoleData(index)
+            return self.displayRoleData(index.row(), index.column())
         return None
 
-    def sort(self, _Ncol, order):
+    def sort(self, Ncol, order):
         self.sortOrder = order
+        if Ncol == 0:
+            self.sortKey = None
+        else:
+            self.sortKey = lambda x: self.__fileInfos[x].get(self.__columnList[Ncol])
         self.refresh()
 
     def fileList(self):
-        fileList = self.__obsLightManager.getPackageParameter(self.__project,
-                                                              self.__package,
-                                                              u"listFile")
-        fileList.sort(reverse=(self.sortOrder == Qt.SortOrder.DescendingOrder))
-        return fileList
+        return self.__fileList
 
     def refresh(self):
+        files = self.__obsLightManager.getPackageParameter(self.__project,
+                                                           self.__package,
+                                                           u"listFile")
+        for fileName in files:
+            inf = self.__obsLightManager.getPackageFileInfo(self.__project,
+                                                            self.__package,
+                                                            fileName)
+            for key in inf.keys():
+                if not key in self.__columnList:
+                    self.__columnList.append(key)
+            self.__fileInfos[fileName] = inf
+        files.sort(reverse=(self.sortOrder == Qt.SortOrder.DescendingOrder),
+                   key=self.sortKey)
+        self.__fileList = files
         self.layoutChanged.emit()
