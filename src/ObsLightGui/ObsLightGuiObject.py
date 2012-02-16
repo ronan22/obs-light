ac6a45ca2ecb50b46144fbe3b4af965e5c4cbdf2
Florent Vennetier <florent@fridu.net>
2012-02-16 17:22:08 +0100

    Cleaned ObsLightGui/ObsLightGuiObject.py

--- src/ObsLightGui/ObsLightGuiObject.py
+++ src/ObsLightGui/ObsLightGuiObject.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 #
-# Copyright 2011, Intel Inc.
+# Copyright 2011-2012, Intel Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,6 +24,10 @@ Created on 19 déc. 2011
 from Utils import ProgressRunnable2
 
 class ObsLightGuiObject(object):
+    """
+    Provides "gui", "manager" and "mainWindow" properties.
+    Provides "callWithProgress" and "callWithInfiniteProgress" methods.
+    """
 
     _gui = None
 
@@ -32,17 +36,33 @@ class ObsLightGuiObject(object):
 
     @property
     def gui(self):
+        """
+        Get a reference to the `ObsLightGui.Gui.Gui` instance.
+        """
         return self._gui
 
     @property
     def manager(self):
+        """
+        Get a reference to the `ObsLight.ObsLightManager.ObsLightManager` instance.
+        """
         return self.gui.getObsLightManager()
 
     @property
     def mainWindow(self):
+        """
+        Get a reference to the main window.
+        """
         return self.gui.mainWindow
 
     def callWithProgress(self, func, iterable, message, *args, **kwargs):
+        """
+        Call `func(item, *args, **kwargs)`, with item being successively
+        each element of `iterable`, while displaying a progress dialog.
+        `message` is displayed in the progress dialog, and if it contains
+        "%(arg)s", it is replaced by a string representation of the item
+        being processed.
+        """
         if len(iterable) > 1:
             progress = self.gui.getProgressDialog()
         else:
@@ -58,6 +78,10 @@ class ObsLightGuiObject(object):
         return runnable.result
 
     def callWithInfiniteProgress(self, func, message, *args, **kwargs):
+        """
+        Call `func(*args, **kwargs)` while displaying an infinite progress
+        dialog with `message`.
+        """
         runnable = ProgressRunnable2(self.gui.getInfiniteProgressDialog())
         runnable.setDialogMessage(message)
         runnable.setRunMethod(func, *args, **kwargs)
