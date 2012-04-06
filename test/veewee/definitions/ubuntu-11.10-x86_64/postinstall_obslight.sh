0bd15332ab6a310e8f759874da15028c415ccee4
Florent Vennetier <florent@fridu.net>
2012-04-06 11:51:55 +0200

    Added VeeWee definitions for Ubuntu 11.10 (WIP)

---
+++ test/veewee/definitions/ubuntu-11.10-x86_64/postinstall_obslight.sh
@@ -0,0 +1,25 @@
+#
+# OBS Light post-installation configuration
+#
+# http://wiki.meego.com/OBS_Light_Installation#For_Ubuntu
+#
+
+# Add vagrant to "users" group
+usermod -a -G users vagrant
+
+# Add packages repositories for OBS Light
+cat > /etc/apt/sources.list.d/obslight.list << EOF
+# Ubuntu 11.10
+## MIC and dependencies
+deb http://download.meego.com/live/devel:/tools:/building/xUbuntu_11.10/ ./
+## OBS Light
+deb http://repo.pub.meego.com/Project:/OBS_Light/Ubuntu_11.10/ ./
+EOF
+apt-add-repository ppa:pyside/ppa
+
+# Refresh package database
+apt-get -y update
+
+# Download OBS Light and its dependencies, without installing them
+apt-get -y -d install obslight
+
