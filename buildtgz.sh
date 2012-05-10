9c01d9470273bdf7a6536cdf08bc6a2841d4358b
Florent Vennetier <florent@fridu.net>
2012-05-10 18:10:03 +0200

    Updated buildtgz.sh and send_to_devel_obs.sh - buildtgz.sh now packages HEAD and not local copy - send_to_devel_obs.sh sends sources to Project:OBS_Light:Testing project

--- buildtgz.sh
+++ buildtgz.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-rm src/dist/obslight*.tar.gz
+INITIALDIR=`pwd`
+TMPDIR=`mktemp -d`
+
+rm -f src/dist/obslight*.tar.gz
+echo "creating archive in $TMPDIR"
+git archive -o $TMPDIR/obslight.tar HEAD
+cd $TMPDIR
+tar -xf obslight.tar
+rm -f src/dist/obslight*.tar.gz
 cd src
 python setup.py sdist
+cp -f dist/obslight*.tar.gz $INITIALDIR/src/dist/
+cd $INITIALDIR
 echo "Compressed tar in src/dist/"
+rm -rf $TMPDIR
 
