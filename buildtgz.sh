648bc39106b2d693e50a242ed037c2e8823a9289
Florent Vennetier <florent@fridu.net>
2012-05-22 17:01:47 +0200

    Updated buildtgz.sh to create an archive of mds-tools too

--- buildtgz.sh
+++ buildtgz.sh
@@ -11,7 +11,14 @@ tar -xf obslight.tar
 rm -f src/dist/obslight*.tar.gz
 cd src
 python setup.py sdist
-cp -f dist/obslight*.tar.gz $INITIALDIR/src/dist/
+cp -vf dist/obslight*.tar.gz $INITIALDIR/src/dist/
+cd ..
+
+MDSVERSION=`sed -rn s,"Version:\s*(.*)$","\1",p mds-tools/mds-tools.spec`
+mv mds-tools mds-tools-$MDSVERSION
+tar -czf mds-tools-$MDSVERSION.tar.gz mds-tools-$MDSVERSION
+cp -vf mds-tools-$MDSVERSION.tar.gz $INITIALDIR/src/dist/mds-tools-$MDSVERSION.tar.gz
+
 cd $INITIALDIR
 echo "Compressed tar in src/dist/"
 rm -rf $TMPDIR
