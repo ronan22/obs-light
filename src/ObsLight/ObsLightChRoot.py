0b9543cf13a81a46ad57a7199e24fd73d88c0d1c
Ronan Le Martret <ronan@fridu.net>
2012-06-29 17:17:19 +0200

    - Remove tmp Build directory at the end of the build.

--- src/ObsLight/ObsLightChRoot.py
+++ src/ObsLight/ObsLightChRoot.py
@@ -762,7 +762,7 @@ chown -R root:users %(buildLink)s/SOURCES/
 chown -R root:users %(buildLink)s/SPECS/
 %(rpmbuildCmd)s
 RPMBUILD_RETURN_CODE=$?
-cp -fpr %(buildDirTmpPath)s/BUILD/* %(buildDir)s/BUILD/
+rm -r %(buildDirTmpPath)s/BUILD/
 rm -r %(buildDirTmpPath)s/TMP
 rm -f %(buildLink)s
 ln -s %(buildDir)s %(buildLink)s
