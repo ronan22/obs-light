a6acda9651ba6b0d309945829ed5da567b4fce96
Ronan Le Martret <ronan@fridu.net>
2012-09-06 09:45:31 +0200

    - Change Wizard

--- obslight/ObsLightGui/Wizard/wizard_chooseLocalProject.py
+++ obslight/ObsLightGui/Wizard/wizard_chooseLocalProject.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/wizard_choosePackage.ui'
+# Form implementation generated from reading ui file 'ui/wizard_chooseLocalProject.ui'
 #
-# Created: Wed Sep  5 11:04:15 2012
+# Created: Wed Sep  5 11:04:14 2012
 #      by: pyside-uic 0.2.13 running on PySide 1.1.0
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,9 +12,10 @@ from PySide import QtCore, QtGui
 class Ui_WizardPage(object):
     def setupUi(self, WizardPage):
         WizardPage.setObjectName("WizardPage")
-        WizardPage.resize(400, 300)
+        WizardPage.resize(430, 338)
         WizardPage.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
         self.formLayout = QtGui.QFormLayout(WizardPage)
+        self.formLayout.setFieldGrowthPolicy(QtGui.QFormLayout.ExpandingFieldsGrow)
         self.formLayout.setObjectName("formLayout")
         self.label = QtGui.QLabel(WizardPage)
         self.label.setObjectName("label")
@@ -22,16 +23,16 @@ class Ui_WizardPage(object):
         self.filterLineEdit = QtGui.QLineEdit(WizardPage)
         self.filterLineEdit.setObjectName("filterLineEdit")
         self.formLayout.setWidget(0, QtGui.QFormLayout.FieldRole, self.filterLineEdit)
-        self.packageListWidget = QtGui.QListWidget(WizardPage)
-        self.packageListWidget.setObjectName("packageListWidget")
-        self.formLayout.setWidget(1, QtGui.QFormLayout.SpanningRole, self.packageListWidget)
+        self.projectListWidget = QtGui.QListWidget(WizardPage)
+        self.projectListWidget.setObjectName("projectListWidget")
+        self.formLayout.setWidget(1, QtGui.QFormLayout.SpanningRole, self.projectListWidget)
 
         self.retranslateUi(WizardPage)
         QtCore.QMetaObject.connectSlotsByName(WizardPage)
 
     def retranslateUi(self, WizardPage):
         WizardPage.setWindowTitle(QtGui.QApplication.translate("WizardPage", "WizardPage", None, QtGui.QApplication.UnicodeUTF8))
-        WizardPage.setTitle(QtGui.QApplication.translate("WizardPage", "Package selection", None, QtGui.QApplication.UnicodeUTF8))
-        WizardPage.setSubTitle(QtGui.QApplication.translate("WizardPage", "Select packages to import.", None, QtGui.QApplication.UnicodeUTF8))
+        WizardPage.setTitle(QtGui.QApplication.translate("WizardPage", "Project selection", None, QtGui.QApplication.UnicodeUTF8))
+        WizardPage.setSubTitle(QtGui.QApplication.translate("WizardPage", "Select a project to import.", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText(QtGui.QApplication.translate("WizardPage", "Filter:", None, QtGui.QApplication.UnicodeUTF8))
 
--- obslight/ObsLightGui/Wizard/wizard_chooseNewOrExistingPackage.py
+++ obslight/ObsLightGui/Wizard/wizard_chooseNewOrExistingPackage.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/wizard_chooseNewOrExistingPackage.ui'
 #
-# Created: Wed Sep  5 08:54:33 2012
+# Created: Wed Sep  5 11:04:14 2012
 #      by: pyside-uic 0.2.13 running on PySide 1.1.0
 #
 # WARNING! All changes made in this file will be lost!
@@ -23,12 +23,12 @@ class Ui_WizardPage(object):
         self.createNewPackageButton = QtGui.QRadioButton(WizardPage)
         self.createNewPackageButton.setObjectName("createNewPackageButton")
         self.verticalLayout.addWidget(self.createNewPackageButton)
-        self.branchPackageButton = QtGui.QRadioButton(WizardPage)
-        self.branchPackageButton.setEnabled(True)
-        self.branchPackageButton.setToolTip("")
-        self.branchPackageButton.setCheckable(True)
-        self.branchPackageButton.setObjectName("branchPackageButton")
-        self.verticalLayout.addWidget(self.branchPackageButton)
+        self.copyPackageButton = QtGui.QRadioButton(WizardPage)
+        self.copyPackageButton.setEnabled(True)
+        self.copyPackageButton.setToolTip("")
+        self.copyPackageButton.setCheckable(True)
+        self.copyPackageButton.setObjectName("copyPackageButton")
+        self.verticalLayout.addWidget(self.copyPackageButton)
 
         self.retranslateUi(WizardPage)
         QtCore.QMetaObject.connectSlotsByName(WizardPage)
@@ -39,5 +39,5 @@ class Ui_WizardPage(object):
         WizardPage.setSubTitle(QtGui.QApplication.translate("WizardPage", "Do you want to import an existing package from server, create a new one from scratch, or branch an existing package on server ?", None, QtGui.QApplication.UnicodeUTF8))
         self.importExistingPackageButton.setText(QtGui.QApplication.translate("WizardPage", "Import existing package", None, QtGui.QApplication.UnicodeUTF8))
         self.createNewPackageButton.setText(QtGui.QApplication.translate("WizardPage", "Create new package", None, QtGui.QApplication.UnicodeUTF8))
-        self.branchPackageButton.setText(QtGui.QApplication.translate("WizardPage", "Copy of an existing package", None, QtGui.QApplication.UnicodeUTF8))
+        self.copyPackageButton.setText(QtGui.QApplication.translate("WizardPage", "Copy of an existing package", None, QtGui.QApplication.UnicodeUTF8))
 
